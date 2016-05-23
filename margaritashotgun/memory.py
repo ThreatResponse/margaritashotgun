7b51a1d1c6851616817499146e5dc772c56bff41
Joel Ferrier <joel@ferrier.io>
2016-05-23 00:06:25 -0700

    include exception handling for socket errors and remote disconnects

--- margaritashotgun/memory.py
+++ margaritashotgun/memory.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from progressbar import ProgressBar, Percentage, Bar, ETA, FileTransferSpeed
+import errno
 import socket
 import s3fs
 
@@ -12,65 +13,104 @@ class memory():
         self.recv_size   = 1024*1024
         self.memsize     = memsize
         padding          = memsize * 0.03
-        self.maxsize     = memsize + padding # pad filesize by 3% to account for lime memdump format
+        self.maxsize     = memsize + padding
         self.transfered  = 0
         self.widgets     = [Percentage(), ' ', Bar(), ' ', ETA(), ' ', FileTransferSpeed()]
 
     def to_file(self, filename):
-        pbar = ProgressBar(widgets=self.widgets, maxval=self.maxsize).start()
-        pbar.start()
-        pbar_update = True
+        self.pbar = ProgressBar(widgets=self.widgets, maxval=self.maxsize).start()
+        self.pbar.start()
+        self.pbar_update = True
 
-        with open(filename, 'wb') as outfile:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect((self.remote_host, self.remote_port))
+        with open(filename, 'wb') as self.outfile:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.remote_host, self.remote_port))
+            self.sock.settimeout(2)
             while True:
-                data = sock.recv(self.recv_size)
-                if not data:
-                    break
-                outfile.write(data)
-                self.transfered = self.transfered + len(data)
-                # this is awful
                 try:
-                    if pbar_update == True:
-                        pbar.update(self.transfered)
-                except Exception as e:
-                    pbar_update = False
-                    print("{}: {} exceeds memsize {}".format(e, self.transfered, self.maxsize))
-            sock.close()
-            outfile.close()
-            if pbar_update == True:
-                pbar.update(self.maxsize)
-                pbar.finish()
+                    data = self.sock.recv(self.recv_size)
+                    data_length = len(data)
+                    if not data:
+                        break
+                    self.outfile.write(data)
+                    self.transfered = self.transfered + data_length
+                    data = None
+                    data_length = 0
+                    try:
+                        if pbar_update == True:
+                            self.pbar.update(self.transfered)
+                    except Exception as e:
+                        pbar_update = False
+                        print("{}: {} exceeds memsize {}".format(e, self.transfered, self.maxsize))
+
+                except (socket.timeout, socket.error) as e:
+                    if isinstance(e, socket.timeout):
+                        break
+                    elif isinstance(e, socket.error):
+                        errorcode = e[0]
+                        if errorcode == errno.EINTR:
+                            pass
+                        else:
+                            self.cleanup()
+                            raise
+                    else:
+                        self.sock.close()
+                        self.outfile.close()
+                        raise
+
+        self.cleanup()
         print('Download complete: {}'.format(filename))
 
     def to_s3(self, key_id, secret_key, bucket, filename):
-        pbar = ProgressBar(widgets=self.widgets, maxval=self.maxsize).start()
-        pbar.start()
+        self.pbar = ProgressBar(widgets=self.widgets, maxval=self.maxsize).start()
+        self.pbar.start()
         pbar_update = True
 
         s3 = s3fs.S3FileSystem(anon=False,
                                key=key_id,
                                secret=secret_key)
-        with s3.open('{}/{}'.format(bucket, filename), 'wb') as outfile:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect((self.remote_host, self.remote_port))
+        with s3.open('{}/{}'.format(bucket, filename), 'wb') as self.outfile:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.remote_host, self.remote_port))
+            self.sock.settimeout(2)
 
             while True:
-                data = sock.recv(self.recv_size)
-                if not data:
-                    break
-                outfile.write(data)
-                self.transfered = self.transfered + len(data)
                 try:
-                    if pbar_update == True:
-                        pbar.update(self.transfered)
-                except Exception as e:
-                    update = False
-                    #TODO: (joel) add verbose check
-                    #print("{}: {} exceeds memsize {}".format(e, self.transfered, self.maxval))
-        if pbar_update == True:
-            pbar.update(self.maxsize)
-            pbar.finish()
-        print('Upload complete: {}/{}'.format(bucket,filename))
+                    data = self.sock.recv(self.recv_size)
+                    data_length = len(data)
+                    if not data:
+                        break
+                    self.outfile.write(data)
+                    self.transfered = self.transfered + data_length
+                    data = None
+                    data_length = 0
+                    try:
+                        if pbar_update == True:
+                            self.pbar.update(self.transfered)
+                    except Exception as e:
+                        pbar_update = False
+                        print("{}: {} exceeds memsize {}".format(e, self.transfered, self.maxval))
+                except (socket.timeout, socket.error) as e:
+                    if isinstance(e, socket.timeout):
+                        break
+                    elif isinstance(e, socket.error):
+                        errorcode = e[0]
+                        if errorcode == errno.EINTR:
+                            pass
+                        else:
+                            self.cleanup()
+                            raise
+                    else:
+                        self.sock.close()
+                        self.outfile.close()
+                        raise
+
+        self.cleanup()
+        print('Upload complete: {}'.format(filename))
+
+    def cleanup(self):
+        self.sock.close()
+        self.outfile.close()
+        self.pbar.update(self.maxsize)
+        self.pbar.finish()
 
