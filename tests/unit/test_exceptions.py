240eef8f419a24eac7d8ce9695675be6bf1c106c
Joel Ferrier <joel@ferrier.io>
2016-06-22 01:30:49 -0700

    start with test coverage

---
+++ tests/unit/test_exceptions.py
@@ -0,0 +1,40 @@
+import pytest
+from paramiko.ssh_exception import SSHException
+from margaritashotgun.exceptions import *
+
+def test_invalid_configuration_error():
+    key = 'foo'
+    value = 'bar'
+    reason = 'not supported'
+    with pytest.raises(InvalidConfigurationError):
+        raise InvalidConfigurationError(key, value, reason)
+
+def test_no_configuration_error():
+    with pytest.raises(NoConfigurationError):
+        raise NoConfigurationError()
+
+def test_authentication_missing_username_error():
+    with pytest.raises(AuthenticationMissingUsernameError):
+        raise AuthenticationMissingUsernameError()
+
+def test_authentication_method_missing_error():
+    with pytest.raises(AuthenticationMethodMissingError):
+        raise AuthenticationMethodMissingError()
+
+def test_ssh_connection_error():
+    inner_exception = SSHException("transport failed")
+    host = 'app01.example.com'
+    with pytest.raises(SSHConnectionError):
+        raise SSHConnectionError(host, inner_exception)
+    assert 1==1
+
+def test_kernel_module_not_found_error():
+    kernel_version = '4.4.9-200.fc22.x86_64'
+    repo_url = 'https://s3.amazonaws.com/lime-modules'
+    with pytest.raises(KernelModuleNotFoundError):
+        raise KernelModuleNotFoundError(kernel_version, repo_url)
+
+def test_lime_retries_exceeded_error():
+    with pytest.raises(LimeRetriesExceededError):
+        raise LimeRetriesExceededError(20)
+
---
+++ tests/unit/test_module.py
@@ -0,0 +1,18 @@
+import pytest
+import logging
+import margaritashotgun
+from margaritashotgun.exceptions import NoConfigurationError
+
+def test_set_stream_handler():
+    name = 'unittest'
+    level = logging.DEBUG
+    fmt = "%(message)s"
+    margaritashotgun.set_stream_logger(name, level, format_string=fmt)
+    logger = logging.getLogger(name)
+    assert logger.name == name
+    assert logger.level == level
+
+def test_create_client():
+    with pytest.raises(NoConfigurationError):
+        margaritashotgun.client()
+
