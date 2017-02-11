ebd818ff2c6918b6fdde3939b83d44a2b009096a
Joel Ferrier <joel@ferrier.io>
2017-02-11 15:55:16 -0800

    additional unit tests

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -41,3 +41,41 @@ def test_lime_retries_exceeded_error():
 def test_memory_capture_output_missing_error():
     with pytest.raises(MemoryCaptureOutputMissingError):
         raise MemoryCaptureOutputMissingError('app01.example.com')
+
+def test_configuration_merge_error():
+    with pytest.raises(ConfigurationMergeError):
+        raise ConfigurationMergeError('example reason')
+
+def test_ssh_command_error():
+    with pytest.raises(SSHCommandError):
+        raise SSHCommandError("172.16.20.1", "cat /proc/net/tcp",
+                              "paramiko internal error message")
+
+def test_repository_error():
+    with pytest.raises(RepositoryError):
+        raise RepositoryError('https://www.example.com/repo/repomd/repodata.xml',
+                              'malformed xml')
+
+def test_repository_missing_key_error():
+    with pytest.raises(RepositoryMissingSigningKeyError):
+        raise RepositoryMissingSigningKeyError('https://www.example.com/repo')
+
+def test_repository_missing_signature_error():
+    with pytest.raises(RepositoryMissingSignatureError):
+        raise RepositoryMissingSignatureError('https://www.example.com/repo/modules/lime-2.6.32-131.0.15.el6.centos.plus.x86_64.ko.sig')
+
+def test_repository_untrusted_signing_key_error():
+    with pytest.raises(RepositoryUntrustedSigningKeyError):
+        raise RepositoryUntrustedSigningKeyError('https://www.example.com/repo/SIGNING_KEY.asc', 'SIGNING_KEY_FINGERPRINT')
+
+def test_repository_signature_error():
+    with pytest.raises(RepositorySignatureError):
+        raise RepositorySignatureError('https://www.example.com/repo/modules/lime-2.6.32-131.0.15.el6.centos.plus.x86_64.ko', 'https://www.example.com/repo/modules/lime-2.6.32-131.0.15.el6.centos.plus.x86_64.ko.sig')
+
+def test_kernel_module_not_provided_error():
+    with pytest.raises(KernelModuleNotProvidedError):
+        raise KernelModuleNotProvidedError('2.6.32-131.0.15.el6.centos.plus.x86_64')
+
+def test_memory_attribute_missing_error():
+    with pytest.raises(MemoryCaptureAttributeMissingError):
+        raise MemoryCaptureAttributeMissingError('local_port')
