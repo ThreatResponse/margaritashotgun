8d1466e679e4317c44bc899f2b74023ac2f40ad8
andrewkrug <andrewkrug@gmail.com>
2017-07-20 20:37:35 -0700

    reflow setup.py

--- setup.py
+++ setup.py
@@ -8,29 +8,35 @@ class install(_install):
         self.run_command('build')
         _install.run(self)
 
-setup(name="margaritashotgun",
-      version="0.4.1",
-      author="Joel Ferrier",
-      author_email="joel@ferrier.io",
-      packages=["margaritashotgun", "margaritashotgun/util"],
-      license="MIT",
-      description="Remote memory aquisition wrapper for LiME",
-      scripts=['bin/margaritashotgun'],
-      url="https://github.com/ThreatResponse/margaritashotgun",
-      download_url="https://github.com/ThreatResponse/margaritashotgun/archive/v0.4.0.tar.gz",
-      use_2to3=True,
-      install_requires=['boto3>=1.3.0',
-                        'python-decouple',
-                        'paramiko>=1.16.0',
-                        'pyyaml>=3.11',
-                        's3fs>=0.0.2',
-                        'progressbar_latest',
-                        'enum34',
-                        'requests',
-                        'xmltodict',
-                        'logutils==0.3.3',
-                        'python-gnupg==0.3.9',
-                        'prompt_toolkit'],
-      tests_require=['pytest',
-                     'pytest-cov',
-                     'mock'])
+setup(
+    name="margaritashotgun",
+    version="0.4.1",
+    author="Joel Ferrier",
+    author_email="joel@ferrier.io",
+    packages=["margaritashotgun", "margaritashotgun/util"],
+    license="MIT",
+    description="Remote memory aquisition wrapper for LiME",
+    scripts=['bin/margaritashotgun'],
+    url="https://github.com/ThreatResponse/margaritashotgun",
+    download_url="https://github.com/ThreatResponse/margaritashotgun/archive/v0.4.1.tar.gz",
+    use_2to3=True,
+    install_requires=[
+        'boto3>=1.3.0',
+        'python-decouple',
+        'paramiko>=1.16.0',
+        'pyyaml>=3.11',
+        's3fs>=0.0.2',
+        'progressbar_latest',
+        'enum34',
+        'requests',
+        'xmltodict',
+        'logutils==0.3.3',
+        'python-gnupg==0.3.9',
+        'prompt_toolkit'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'mock'
+    ]
+)
